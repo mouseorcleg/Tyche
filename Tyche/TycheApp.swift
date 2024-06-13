680b89afeed9fd3252dcd83c4a79741bc226bdf5
Maria Kharybina <maria.karybina@bbc.co.uk>
2024-06-13 11:07:39 +0100

    Initial Commit

---
+++ Tyche/TycheApp.swift
@@ -0,0 +1,17 @@
+//
+//  TycheApp.swift
+//  Tyche
+//
+//  Created by Maria Kharybina on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TycheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
